dc5228bc8bdb7ac1d679659888635c534d331e32
HY <hanyikoh@gmail.com>
2021-03-12 13:17:43 +0800

    set up Main Page

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "QuizApp",
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home:,
+    );
+  }
+}
