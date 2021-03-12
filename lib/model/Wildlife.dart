52005a16d00210786fd755dd71a553f4f7d9f130
Shi Jing <shijingkoh@outlook.com>
2021-03-12 20:32:18 +0800

    add detail page

---
+++ lib/model/Wildlife.dart
@@ -0,0 +1,18 @@
+class Wildlife {
+  String name;
+  String fact;
+  String imagehref;
+  String status;
+  List habitats;
+  List countries;
+  List threats;
+
+  Wildlife(
+      {this.name,
+      this.fact,
+      this.imagehref,
+      this.status,
+      this.habitats,
+      this.countries,
+      this.threats});
+}
--- lib/module/listpage.dart
+++ lib/module/listpage.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:wildlifecare/widget/drawer.dart';
+import 'package:wildlifecare/model/Wildlife.dart';
+import 'package:wildlifecare/module/detailpage.dart';
 
-class ListPage extends StatelessWidget {
-  // final _saved = <Wildlife>{};
-  static const String routeName = '/list';
+class ListPage extends StatefulWidget {
+  static const String routeName = "/list";
+  @override
+  _ListPageState createState() => _ListPageState();
+}
+
+class _ListPageState extends State<ListPage> {
+  final _saved = <Wildlife>{};
+  List wildlives;
+
+  @override
+  void initState() {
+    wildlives = getWildlives();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,29 +29,40 @@ class ListPage extends StatelessWidget {
         ],
       ),
       drawer: DrawerNav(),
-      body: ListView(
-        children: <Widget>[
-          _createWildlife(),
-          _createWildlife(),
-        ],
+      body: Container(
+        child: ListView.builder(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          itemCount: wildlives.length,
+          itemBuilder: (BuildContext context, int index) {
+            return _createWildlife(wildlives[index]);
+          },
+        ),
       ),
     );
   }
 
   void _pushSaved() {}
 
-  Widget _createWildlife() {
+  Widget _createWildlife(Wildlife wl) {
     return Card(
+      elevation: 8.0,
+      margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
       child: Padding(
         padding: EdgeInsets.all(16.0),
         child: InkWell(
-          onTap: () => {},
+          onTap: () => {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => DetailPage(wildlife: wl)))
+          },
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               Column(children: [
                 Text(
-                  "Number - Name",
+                  wl.name,
                   style: TextStyle(fontSize: 20.0),
                 ),
                 Row(
@@ -45,15 +70,15 @@ class ListPage extends StatelessWidget {
                     Container(
                       padding: EdgeInsets.all(10.0),
                       margin: const EdgeInsets.all(10.0),
-                      child: Text("Char. 1"),
+                      child: Text(wl.countries[0]),
                       decoration: BoxDecoration(
                           border: Border.all(width: 1),
-                          borderRadius: BorderRadius.circular(12)),
+                          borderRadius: BorderRadius.circular(10)),
                     ),
                     Container(
                       padding: EdgeInsets.all(10.0),
                       margin: const EdgeInsets.all(10.0),
-                      child: Text("Charact. 2"),
+                      child: Text(wl.countries[1]),
                       decoration: BoxDecoration(
                           border: Border.all(width: 1),
                           borderRadius: BorderRadius.circular(12)),
@@ -70,5 +95,56 @@ class ListPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  List getWildlives() {
+    return [
+      Wildlife(
+          name: "ABUDEN",
+          fact:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
+          imagehref:
+              "https://www.google.com/url?sa=i&url=https%3A%2F%2Fhatrabbits.com%2Fen%2Frandom-image%2F&psig=AOvVaw0jeTm9UpifIaA6wUG8gznN&ust=1615633233068000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKDol_rMqu8CFQAAAAAdAAAAABAD",
+          status: "ITAKTAU",
+          habitats: [
+            "Forest - Subtropical/Tropical Moist Montane"
+          ],
+          countries: [
+            "Colombia",
+            "Ecuador",
+            "Peru",
+            "Venezuela, Bolivarian Republic of"
+          ],
+          threats: [
+            "Annual & perennial non-timber crops",
+            "Agro-industry farming",
+            "Livestock farming & ranching",
+            "Agro-industry grazing, ranching or farming",
+            "Work & other activities"
+          ]),
+      Wildlife(
+          name: "ABUDEN",
+          fact:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+          imagehref:
+              "https://www.google.com/url?sa=i&url=https%3A%2F%2Fhatrabbits.com%2Fen%2Frandom-image%2F&psig=AOvVaw0jeTm9UpifIaA6wUG8gznN&ust=1615633233068000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKDol_rMqu8CFQAAAAAdAAAAABAD",
+          status: "ITAKTAU",
+          habitats: [
+            "Forest - Subtropical/Tropical Moist Montane"
+          ],
+          countries: [
+            "Colombia",
+            "Ecuador",
+            "Peru",
+            "Venezuela, Bolivarian Republic of"
+          ],
+          threats: [
+            "Annual & perennial non-timber crops",
+            "Agro-industry farming",
+            "Livestock farming & ranching",
+            "Agro-industry grazing, ranching or farming",
+            "Work & other activities"
+          ]),
+    ];
   }
 }
